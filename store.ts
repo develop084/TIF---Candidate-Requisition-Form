1ea2930faa9a6524316fced28b51d063886faa2e
develop084 <patidardev084@gmail.com>
2023-06-22 06:28:04 +0000

    removed counter slice

--- store.ts
+++ store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '@src/features/counterSlice';
 import requisitionSlice from '@src/features/requisitionSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     requisition: requisitionSlice
   },
 });
